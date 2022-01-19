8a9de98130789f2e30cbd25a017995e57d062187
RicardoIT-Web <rick_piedade@hotmail.com>
2022-01-19 11:02:21 +0000

    add further tests and test with jest

--- scripts/game.test.js
+++ scripts/game.test.js
@@ -2,10 +2,6 @@
  * @jest-environment jsdom
  */
 
-const { expect } = require("@jest/globals");
-const { beforeAll } = require("jest-circus");
-const { test } = require("picomatch");
-const { describe } = require("yargs");
 const { game } = require("../scripts/game");
 
 beforeAll(() => {
@@ -19,5 +15,17 @@ beforeAll(() => {
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
