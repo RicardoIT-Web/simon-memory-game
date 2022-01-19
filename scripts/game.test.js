7f6f5090daef066efe065e3ef6239b41c77679f8
RicardoIT-Web <rick_piedade@hotmail.com>
2022-01-19 13:07:03 +0000

    add newGame function

--- scripts/game.test.js
+++ scripts/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../scripts/game");
+const { game, newGame } = require("../scripts/game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -27,5 +27,15 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("should set game score to 0", () => {
+        expect(game.score).toEqual(0);
     });
 });
